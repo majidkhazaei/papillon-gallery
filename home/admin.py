9cd4f93839327e6edff8bc92850f277fa5651cae
Majid <majidtriste1@gmail.com>
2026-06-20 10:22:21 +0330

    feat(admin): customize Django admin panel titles
    - Set custom admin site header
    - Set custom admin site title
    - Set custom admin index title

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+admin.site.site_header = "papillon art gallery"
+admin.site.site_title = "papillon art gallery"
+admin.site.index_title = "welcome to my gallery"
