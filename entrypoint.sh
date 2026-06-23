dbc5310633e6d397b137510c2114c5bc1839af2b
Majid <majidtriste1@gmail.com>
2026-06-23 22:51:44 +0330

    chore: final production readiness with celery, redis, and nginx fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
 echo "Waiting for DB..."
-
 while ! nc -z db 5432; do
   sleep 1
 done
-
 echo "DB ready!"
-
 
 echo "Waiting for Redis..."
 while ! nc -z redis 6379; do
@@ -20,4 +17,9 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 
-gunicorn A.wsgi:application --bind 0.0.0.0:8000 --workers 3
+if [ $# -eq 0 ]; then
+
+    exec gunicorn A.wsgi:application --bind 0.0.0.0:8000 --workers 3
+else
+    exec "$@"
+fi
